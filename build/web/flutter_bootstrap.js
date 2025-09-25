1433b95f5d458d5aa761dfe8d5a5235e9e4c9b43
Wilma P. <wilma.paloheimo@taigoa.fi>
2025-09-25 15:15:43 +0300

    ?

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3325725524"
+    serviceWorkerVersion: "1715688805"
   }
 });
